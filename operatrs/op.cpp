1f0f0e2c2b746abb646fbacd0654e9e2bd57adc6
milansir <rwn3developer11@gmail.com>
2026-07-18 18:01:44 +0530

    operators-18/7/26

---
+++ operatrs/op.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	//int a = 10, b = 3;
+	//Arithmetic Operators
+//	cout << a + b << endl; // 13
+//	cout << a - b << endl; // 7
+//	cout << a * b << endl; // 30
+//	cout << a / b << endl; // 3
+//	cout << a % b << endl; // 1
+	
+	//Assignment Operators
+//	int a=10;
+//	a%=5;
+//	cout<<"Ans :- "<<a<<endl;
+
+//	Comparison Operators
+//	int a=10,b=10;
+//	cout<<"Ans :- "<<(a > b)<<endl;
+//	cout<<"Ans :- "<<(a < b)<<endl;
+//	cout<<"Ans :- "<<(a >= b)<<endl;
+//	cout<<"Ans :- "<<(a <= b)<<endl;
+//	cout<<"Ans :- "<<(a == b)<<endl;
+//	cout<<"Ans :- "<<(a != b)<<endl;
+
+	//logical operator
+	//&&   ||   !
+	
+	return 0;
+}
